918da902f0b41b3ce7e25731d214bb8bd237b31f
Pablo Benjamin Heredia Ruiz <pbheredia@udabol.edu.bo>
2026-04-23 20:20:13 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+
+@Module({
+  imports: [],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class AppModule {}
